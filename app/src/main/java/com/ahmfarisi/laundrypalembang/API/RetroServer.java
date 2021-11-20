9b44e6b7c6d3d6e62c54aa5197d6d73d8cfcb634
tantowi-17 <tantowibowo7@gmail.com>
2021-11-20 21:08:16 +0800

    [EN-01] Update local server

--- app/src/main/java/com/ahmfarisi/laundrypalembang/API/RetroServer.java
+++ app/src/main/java/com/ahmfarisi/laundrypalembang/API/RetroServer.java
@@ -4,8 +4,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-    private static final String baseURL = "https://tantowibowo.com/nikah/";
-//    private static final String baseURL = "http://10.0.2.2/laundry/";
+//    private static final String baseURL = "https://tantowibowo.com/nikah/"; // server prodaction
+    private static final String baseURL = "http://10.0.2.2/nikah/"; // server local
     private static Retrofit retro;
 
     public static Retrofit konekRetrofit(){
